c49322cd60b5632e5be91ba81c9fb71589fcd7f8
Harsh Vaddoriya <128826276+harshvaddoriya@users.noreply.github.com>
2025-08-06 18:22:28 +0530

    add changes

--- src/app/reels/page.js
+++ src/app/reels/page.js
@@ -1,11 +1,11 @@
 import Downloader from "@/components/downloader/Downloader";
 import React from "react";
-import DownloadDescription from "@/components/common/DownloadDescription/DownloadDescription";
-import AppPromotion from "@/components/common/AppPromotion/AppPromotion";
-import FaqSection from "@/components/common/Faq/Faq";
+import DownloadDescription from "@/common/DownloadDescription/DownloadDescription";
+import AppPromotion from "@/common/AppPromotion/AppPromotion";
+import FaqSection from "@/common/Faq/Faq";
 import Images from "../../../public/images/index";
 import { reelsSteps, reelsFaq } from "../../dataStore/data";
-import AboutProcess from "@/components/common/AboutProcess/AboutProcess";
+import AboutProcess from "@/common/AboutProcess/AboutProcess";
 
 export const metadata = {
   title: "Reels Downloader",
