bc7657d4588331c4335afc93111a13017c958236
ravishadgm <ravishapatel.dgm@gmail.com>
2025-08-12 11:52:49 +0530

    seo fix

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,11 +3,32 @@ import Header from "@/layout/Header/Page";
 import Footer from "@/layout/Footer/Page";
 import "../styles/globals.scss";
 
-
 export default function RootLayout({ children }) {
+  const schema = {
+    "@context": "https://schema.org",
+    "@type": "Organization",
+    "name": "InstaDL",
+    "url": "https://video-downloader-sigma-seven.vercel.app/",
+    "contactPoint": [
+      {
+        "@type": "ContactPoint",
+        "email": "abc@gmail.com",
+        "contactType": "customer support",
+        "availableLanguage": ["English"]
+      }
+    ]
+  };
+
   return (
     <html lang="en">
-      <head></head>
+      <head>
+        <Script
+          id="organization-schema"
+          type="application/ld+json"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{ __html: JSON.stringify(schema) }}
+        />
+      </head>
       <body className="layout-wrapper">
         <Header />
         {children}
